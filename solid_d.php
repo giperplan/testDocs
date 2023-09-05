fffbcabab81a9a5db07ad2d0735594da5e3abd6a
Yurii Kliushnykov <x157157157@gmail.com>
2023-09-05 21:20:11 +0400

    Fix DIP

--- solid_d.php
+++ solid_d.php
@@ -1,11 +1,29 @@
 <?php
 
-class XMLHttpService extends XMLHTTPRequestService {}
+class XMLHTTPRequestService {
+    public function request(string $url, string $method, array $options) {
+        
+    }
+}
+
+interface HttpServiceInterface {
+    // Add realizations by necessity
+    
+    // public function request(string $url, string $method, array $options);
+}
+
+class XMLHttpService implements HttpServiceInterface {
+    
+}
+
 
 class Http {
     private $service;
-
-    public function __construct(XMLHttpService $xmlHttpService) { }
+    
+                                // Replacing dependence with abstraction
+    public function __construct(HttpServiceInterface  $xmlHttpService) {
+        $this->service = $xmlHttpService;
+    }
 
     public function get(string $url, array $options) {
         $this->service->request($url, 'GET', $options);
