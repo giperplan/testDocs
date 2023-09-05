624a08925e698f050448a523348bd065ef71709f
Yurii Kliushnykov <x157157157@gmail.com>
2023-09-05 20:42:40 +0400

    Fix handleObjects on the principle OCL

--- solid_o.php
+++ solid_o.php
@@ -3,23 +3,32 @@
 class SomeObject {
     protected $name;
 
-    public function __construct(string $name) { }
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
 
-    public function getObjectName() { }
+    public function getObjectName() { 
+        return $this->name;
+    }
 }
 
 class SomeObjectsHandler {
+    private $handlersList = [
+        'object_1' => 'handle_object_1',
+        'object_2' => 'handle_object_2'
+    ];
+    
     public function __construct() { }
+    
+    protected function setHandlersList(array $handlersList) {
+        $this->handlersList = $handlersList;
+    }
 
     public function handleObjects(array $objects): array {
         $handlers = [];
-        foreach ($objects as $object) {
-            if ($object->getObjectName() == 'object_1')
-                $handlers[] = 'handle_object_1';
-            if ($object->getObjectName() == 'object_2')
-                $handlers[] = 'handle_object_2';
+        foreach ($objects as $object) {            
+            $handlers[] = $this->handlersList[$object->getObjectName()];            
         }
-
         return $handlers;
     }
 }
@@ -30,4 +39,23 @@ $objects = [
 ];
 
 $soh = new SomeObjectsHandler();
-$soh->handleObjects($objects);
+var_dump($soh->handleObjects($objects));
+
+/**
+ * Now we can add new objects and handlers without changing SomeObjectsHandler class
+ */
+class NewObjectsHandler extends SomeObjectsHandler {    
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->setHandlersList([
+            'object_1' => 'handle_object_3',
+            'object_2' => 'handle_object_4',
+        ]);
+    } 
+}
+
+echo "check new handler";
+$noh = new NewObjectsHandler();
+var_dump($noh->handleObjects($objects));
